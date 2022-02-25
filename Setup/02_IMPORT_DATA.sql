e9672ee83c0a729aa605b2f5343807a06e43b589
Brian <brian@topcoinx.com>
2022-02-25 12:53:45 +0800

    Add table schema and populate default data into DB

---
+++ Setup/02_IMPORT_DATA.sql
@@ -0,0 +1,18 @@
+INSERT INTO USER (USER_NAME, FIRST_NAME, LAST_NAME, PASSWORD)
+VALUES ('brian', 'Lai', 'Brian', ''); -- Password = Hello
+INSERT INTO USER (USER_NAME, FIRST_NAME, LAST_NAME, PASSWORD)
+VALUES ('janet', 'Chan', 'Janet', ''); -- Password = World
+INSERT INTO USER (USER_NAME, FIRST_NAME, LAST_NAME, PASSWORD)
+VALUES ('Anna', 'Wong', 'Anna', ''); -- Password = Apple
+INSERT INTO USER (USER_NAME, FIRST_NAME, LAST_NAME, PASSWORD)
+VALUES ('peter', 'Lee', 'Peter', ''); -- Password = Juice
+
+
+INSERT INTO PIZZA (NAME, DESCRIPTION, PRICE)
+VALUES ('Neapolitan Pizza', 'The true original, topped with sauce from fresh tomatoes olive oil and minimal mozzarella.', 99); 
+INSERT INTO PIZZA (NAME, DESCRIPTION, PRICE)
+VALUES ('Chicago Pizza', 'Prepared as the thick, classic deep-dish pizza. Thin-medium crust containing cornmeal or semolina.', 129); 
+INSERT INTO PIZZA (NAME, DESCRIPTION, PRICE)
+VALUES ('New-York Pizza', 'Large, foldable slices, crispy outer crust. Traditional toppings of tomato sauce and mozzarella cheese.', 139); 
+INSERT INTO PIZZA (NAME, DESCRIPTION, PRICE)
+VALUES ('California Pizza', 'A single-serving, thin crust pizza. Popular due to its creative, non-traditional toppings, like chicken, egg, artichokes, salmon, feta or goat cheese.', 129); 
