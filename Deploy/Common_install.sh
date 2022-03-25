676d25bf38f175d0d602e426cc03c7d69230cf41
Brian Lai <brianlaihkhk@gmail.com>
2022-03-25 12:51:23 +0800

    Update Common_install.sh

--- Deploy/Common_install.sh
+++ Deploy/Common_install.sh
@@ -7,7 +7,3 @@ mkdir ../Common/target/lib/
 cd ../Common/
 
 mvn clean package install
-
-# cp ../../tpx-engine-exchange-core/target/exchange-core-0.4.4-SNAPSHOT-jar-with-dependencies.jar ../../tpx-engine-backend/target/lib/
-
-# mvn install:install-file -Dfile=../../tpx-engine-backend/target/lib/exchange-core-0.4.4-SNAPSHOT-jar-with-dependencies.jar -DgroupId=com.pizzahouse.common -DartifactId=exchange-core -Dversion=0.2-SNAPSHOT -Dpackaging=jar -DgenerationPom=true
